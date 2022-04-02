e144913c2f8481f54eddc0346bfc34336d74f349
Srijan Shovit <srijanshovit@gmail.com>
2022-04-03 03:01:39 +0530

    First push

---
+++ airboardprojectait/public/javascripts/index.js
@@ -0,0 +1,32 @@
+function _(selector){
+    return document.querySelector(selector);
+}
+
+function setup(){
+    let canvas = createCanvas(650,600);
+    canvas.parent("#canvas-wrapper");
+    background(255);
+}
+
+function mouseDragged(){
+    let type= "pencil";
+    let size = parseInt(_("#pen-size").value);
+    let color= _("#pen-color").value;
+    fill(color);
+    stroke(color);
+    if(type == "pencil"){
+        strokeWeight(size)
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}
+
+_("#reset-canvas").addEventListener("click",
+function(){
+    background(255);
+});
+
+_("#save-canvas").addEventListener("click",
+function(){
+    saveCanvas(canvas,"sketch","png");
+});
+
