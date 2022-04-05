e04ab9290085422ea4eb568b3d3c15a8bb8df6fc
Srijan Shovit <srijanshovit@gmail.com>
2022-04-05 14:14:06 +0530

    commit 4

--- airboardprojectait/app.js
+++ airboardprojectait/app.js
@@ -1,43 +1,44 @@
 const express=require("express")
-const mongoose=require('mongoose');
-const dotenv = require('dotenv')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-require('./config/passport')(passport)
+require('dotenv').config();
 
+// const mongoose=require('mongoose')
+// const passport = require('passport')
+// const session = require('express-session')
+// const MongoStore = require('connect-mongo')
+// require('dotenv').config({ path: './config/config.env' })
+// require("./config/passport")(passport)
 
 const app=express()
 app.set('view engine','ejs');
 // const PORT = process.env.PORT||3000;
-dotenv.config({ path: './config/config.env' })
 
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-})
+
+// mongoose.connect(process.env.MONGO_URI,{
+//     useNewUrlParser:true,
+//     useUnifiedTopology: true
+// })
 app.use(express.static(__dirname + '/public'));
 // app.use(express.static('public'))
 
 //*-------------------------session middleware--------------------------------
-app.use(express.urlencoded({extended:true}))
-app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-    })
-  )
-  // Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(express.urlencoded({extended:true}))
+// app.use(
+//     session({
+//       secret: 'keyboard cat',
+//       resave: false,
+//       saveUninitialized: false,
+//       store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+//     })
+//   )
+//   // Passport middleware
+// app.use(passport.initialize())
+// app.use(passport.session())
 
 //*---------------------------------------------------------
 
 //*-----------Importing  routes-------------
-app.use(require("./routes/index"))
-app.use('/auth', require('./routes/auth'))
+// app.use(require("./routes/index"))
+// app.use('/auth', require('./routes/auth'))
 //*-----------------------------------------
 
 
@@ -46,7 +47,15 @@ app.use('/auth', require('./routes/auth'))
 app.get("/", (req, res) => {
   // res.render("paint",{title:"Airboard"})
   // res.render("track")
-  res.render("login");
+  // res.render("home")s
+  res.render("teams");
+})
+app.get("/signup",(req, res) => {
+  res.render("signup",{apiurl:process.env.API_URL})
 })
 
-// app.listen(3000)
+app.get("/login",(req, res) => {
+  res.render("login",{apiurl:process.env.API_URL})
+})
+
+app.listen(3300)
